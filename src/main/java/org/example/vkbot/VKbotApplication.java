8a9cd44177d74be9049b136dc601335814cbfa8e
titan <a@a.a>
2025-04-12 21:54:21 +0300

    init

---
+++ src/main/java/org/example/vkbot/VKbotApplication.java
@@ -0,0 +1,13 @@
+package org.example.vkbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VKbotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VKbotApplication.class, args);
+    }
+
+}
